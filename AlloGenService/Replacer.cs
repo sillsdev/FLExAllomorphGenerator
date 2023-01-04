4d2c2e945279e397845cf6ac293163efda48e823
andy black <blackhandrew@gmail.com>
2023-01-04 08:50:58 -0700

    Add replacer

---
+++ AlloGenService/Replacer.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) 2023 SIL International
+// This software is licensed under the LGPL, version 2.1 or later
+// (http://www.gnu.org/licenses/lgpl-2.1.html)
+
+using SIL.AlloGenModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SIL.AlloGenService
+{
+    public class Replacer
+    {
+        public List<Replace> ReplaceOps { get; set; }
+
+        public Replacer(List<Replace> replaceOps)
+        {
+            ReplaceOps = replaceOps;
+        }
+
+        public string ApplyReplaceOpToOneDialect(string input, Dialect dialect)
+        {
+            string result = input;
+            foreach (Replace replace in ReplaceOps)
+            {
+                if (!replace.Active)
+                    continue;
+                switch (dialect)
+                {
+                    case Dialect.Ach:
+                        if (!replace.Ach)
+                            continue;
+                        break;
+                    case Dialect.Acl:
+                        if (!replace.Acl)
+                            continue;
+                        break;
+                    case Dialect.Akh:
+                        if (!replace.Akh)
+                            continue;
+                        break;
+                    case Dialect.Akl:
+                        if (!replace.Akl)
+                            continue;
+                        break;
+                    case Dialect.Ame:
+                        if (!replace.Ame)
+                            continue;
+                        break;
+                }
+                if (replace.Mode)
+                {
+                    // regular expression
+                    result = Regex.Replace(result, replace.From, replace.To);
+                }
+                else
+                {
+                    // plain
+                    result = result.Replace(replace.From, replace.To);
+                }
+            }
+            return result;
+        }
+    }
+
+    public enum Dialect
+    {
+        Ach,
+        Acl,
+        Akh,
+        Akl,
+        Ame,
+    }
+}
---
+++ AlloGenServiceTests/ReplacerTests.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) 2023 SIL International
+// This software is licensed under the LGPL, version 2.1 or later
+// (http://www.gnu.org/licenses/lgpl-2.1.html)
+
+using NUnit.Framework;
+using SIL.AlloGenModel;
+using SIL.AlloGenService;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIL.AlloGenServiceTest
+{
+    public class ReplacerTests
+    {
+        XmlBackEndProvider provider = new XmlBackEndProvider();
+        string TestDataDir { get; set; }
+        string AlloGenFile { get; set; }
+        string AlloGenExpected { get; set; }
+        Replacer replacer { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            Uri uriBase = new Uri(Assembly.GetExecutingAssembly().CodeBase);
+            var rootdir = Path.GetDirectoryName(Uri.UnescapeDataString(uriBase.AbsolutePath));
+            int i = rootdir.LastIndexOf("AlloGenServiceTests");
+            string basedir = rootdir.Substring(0, i);
+            TestDataDir = Path.Combine(basedir, "AlloGenServiceTests", "TestData");
+            AlloGenExpected = Path.Combine(TestDataDir, "AlloGenReplace.agf");
+        }
+
+        [Test]
+        public void ReplaceTest()
+        {
+            provider.LoadDataFromFile(AlloGenExpected); 
+            AllomorphGenerators allomorphGenerators = provider.AlloGens;
+            Assert.NotNull(allomorphGenerators);
+            Operation operation = allomorphGenerators.Operations[0];
+            replacer = new Replacer(operation.Action.ReplaceOps);
+            string input = "";
+            input = "*a:";
+            compareResult(input, "a", Dialect.Ach);
+            compareResult(input, "a", Dialect.Acl);
+            compareResult(input, "a", Dialect.Akh);
+            compareResult(input, "a", Dialect.Akl);
+            compareResult(input, "aa", Dialect.Ame);
+            input = "*arka:";
+            compareResult(input, "arka", Dialect.Ach);
+            compareResult(input, "arka", Dialect.Acl);
+            compareResult(input, "arka", Dialect.Akh);
+            compareResult(input, "arka", Dialect.Akl);
+            compareResult(input, "arkaa", Dialect.Ame);
+            input = "*chillinya:";
+            compareResult(input, "chillinya", Dialect.Ach);
+            compareResult(input, "chillinya", Dialect.Acl);
+            compareResult(input, "chillinya", Dialect.Akh);
+            compareResult(input, "chillinya", Dialect.Akl);
+            compareResult(input, "chillinyaa", Dialect.Ame);
+            input = "*yarqa:.v2";
+            compareResult(input, "yarqa", Dialect.Ach);
+            compareResult(input, "yarqa", Dialect.Acl);
+            compareResult(input, "yarqa", Dialect.Akh);
+            compareResult(input, "yarqa", Dialect.Akl);
+            compareResult(input, "yarqaa", Dialect.Ame);
+            input = "+yusulpa:";
+            compareResult(input, "yusulpa", Dialect.Ach);
+            compareResult(input, "yusulpa", Dialect.Acl);
+            compareResult(input, "yusulpa", Dialect.Akh);
+            compareResult(input, "yusulpa", Dialect.Akl);
+            compareResult(input, "yusulpaa", Dialect.Ame);
+        }
+
+        private void compareResult(string input, string expected, Dialect dialect)
+        {
+            string result = replacer.ApplyReplaceOpToOneDialect(input, dialect);
+            Assert.AreEqual(expected, result);
+
+        }
+    }
+}
